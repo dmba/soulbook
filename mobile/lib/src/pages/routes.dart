379ef61e829648eb37584f395742b73b75069e00
Dmytro Babiienko <dmytro.babiienko@gmail.com>
2019-01-17 12:58:36 +0200

    Initial commit 🎉

---
+++ mobile/lib/src/pages/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/src/pages/home/home.dart';
+
+const HOME = '/';
+
+final routes = <String, WidgetBuilder>{
+  HOME: (BuildContext context) => const HomePage()
+};

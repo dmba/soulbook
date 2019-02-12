91519ffbba676985ac55fd333f0904fe5bf232d9
Dmytro Babiienko <dmytro.babiienko@gmail.com>
2019-02-12 21:43:47 +0200

    Add fire auth deps for android

--- mobile/lib/src/di/ui_module.dart
+++ mobile/lib/src/di/ui_module.dart
@@ -1,7 +1,10 @@
 import 'package:core/core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kiwi/kiwi.dart';
 import 'package:mobile/src/pages/home/home.dart';
 import 'package:mobile/src/pages/intro/intro.dart';
+import 'package:mobile/src/pages/login/auth.dart';
 import 'package:mobile/src/pages/login/login.dart';
 
 part 'package:mobile/src/di/ui_module.g.dart';
@@ -12,9 +15,19 @@ abstract class UiModule implements Module {
   factory UiModule.create() => new _$UiModule();
 
   @override
-  void configure(container) {
+  void configure(Container container) {
+    _configureInstances(container);
+    _configureAuth();
     _configurePages();
   }
+
+  void _configureInstances(Container container) {
+    container.registerInstance(FirebaseAuth.instance);
+    container.registerInstance(GoogleSignIn());
+  }
+
+  @Register.factory(Auth)
+  void _configureAuth();
 
   @Register.factory(HomePage)
   @Register.factory(IntroPage)

f1137f9d5925b61976daf0ee715727b3f7c5a316
Dmytro Babiienko <dmytro.babiienko@gmail.com>
2019-02-18 13:32:23 +0200

    Add menu widget

---
+++ mobile/lib/src/widgets/menu_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/src/model/menu_item.dart';
+
+class MenuWidget extends StatelessWidget {
+  const MenuWidget({
+    Key key,
+    @required this.accountName,
+    @required this.accountEmail,
+    @required this.accountAvatar,
+    @required this.menuItems,
+  }) : super(key: key);
+
+  final String accountName;
+  final String accountEmail;
+  final String accountAvatar;
+  final List<MenuItem> menuItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final _menuTiles = <Widget>[
+      UserAccountsDrawerHeader(
+        accountName: Text(accountName),
+        accountEmail: Text(accountEmail),
+        currentAccountPicture: CircleAvatar(
+          backgroundColor: Colors.blue,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(accountAvatar),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ];
+    _menuTiles.addAll(menuItems.map(_mapToListTile(context)));
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: _menuTiles,
+      ),
+    );
+  }
+
+  _mapToListTile(BuildContext context) {
+    return (MenuItem item) => ListTile(
+          title: Text(item.title),
+          subtitle: Text(item.description),
+          onTap: () => Navigator.pushNamed(context, item.route),
+        );
+  }
+}
